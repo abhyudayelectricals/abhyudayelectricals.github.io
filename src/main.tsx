e26eb018d34325dca7ddc632bd7aa769ea878e03
Abhyuday Pratap Singh <abhyudaypratap@outlook.com>
2024-11-24 22:56:05 +0100

    testing hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.tsx';
 import About from './pages/About.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
